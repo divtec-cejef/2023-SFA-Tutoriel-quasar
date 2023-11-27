4947c98f6c7ec5fa102eba6f5830d8bdc5f951aa
drafolin <derg@drafolin.ch>
2023-11-27 09:02:43 +0100

    Ajout des routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,12 +2,16 @@ import { RouteRecordRaw } from 'vue-router'
 import MainLayout from 'layouts/MainLayout.vue'
 import IndexPage from 'pages/IndexPage.vue'
 import ErrorNotFound from "pages/ErrorNotFound.vue"
+import HelloPage from 'pages/HelloPage.vue'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: MainLayout,
-    children: [{ path: '', component: IndexPage }]
+    children: [
+      { path: '', component: IndexPage },
+      { path: 'hello', component: HelloPage }
+    ]
   },
 
   // Always leave this as last one,
