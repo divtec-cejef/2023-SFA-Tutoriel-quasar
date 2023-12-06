438d20de153ff9b25bfccb9396b6c190f246fd20
drafolin <derg@drafolin.ch>
2023-12-06 15:44:23 +0100

    testing gitbook integration

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,25 +1,26 @@
-import { RouteRecordRaw } from 'vue-router'
-import MainLayout from 'layouts/MainLayout.vue'
-import IndexPage from 'pages/IndexPage.vue'
-import ErrorNotFound from "pages/ErrorNotFound.vue"
-import HelloPage from 'pages/HelloPage.vue'
+import { RouteRecordRaw } from 'vue-router';
+import MainLayout from 'layouts/MainLayout.vue';
+import IndexPage from 'pages/IndexPage.vue';
+import ErrorNotFound from "pages/ErrorNotFound.vue";
+import HelloPage from 'pages/HelloPage.vue';
 
 const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    component: MainLayout,
-    children: [
-      { path: '', component: IndexPage },
-      { path: 'hello', component: HelloPage }
-    ]
-  },
+	{
+		path: '/',
+		component: MainLayout,
+		children: [
+			// Testing gitbook integration
+			{ path: '', component: IndexPage },
+			{ path: 'hello', component: HelloPage }
+		]
+	},
 
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: ErrorNotFound
-  }
-]
+	// Always leave this as last one,
+	// but you can also remove it
+	{
+		path: '/:catchAll(.*)*',
+		component: ErrorNotFound
+	}
+];
 
-export default routes
+export default routes;
